2515f63def9b8dfc075dcbc92725352f9e1c65fc
ishowrunes <ishworisgod@gmail.com>
2025-02-08 21:55:34 +0545

    Add console colors

---
+++ lib/src/console_color.dart
@@ -0,0 +1,17 @@
+enum ConsoleColor {
+  black(0),
+  red(196),
+  green(34),
+  yellow(226),
+  blue(12),
+  magenta(201),
+  cyan(39),
+  white(15),
+  orange(208),
+  purple(93),
+  violet(129);
+
+  const ConsoleColor(this.code);
+
+  final int code;
+}
